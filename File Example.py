759e57b0b5d28ec9e503628bcb6f3f3b9a9244c8
Emanaws <redwanomer44@gmail.com>
2020-06-24 11:46:12 -0500

    File Example

---
+++ File Example.py
@@ -0,0 +1,11 @@
+import os
+#os.mkdir('week4') this to create Dirctarey
+#os.mkdir('week5')
+#os.rmdir('week5') this is to remove dir
+#os.makedirs('Student/Grade/8thGrade/9thGrade/10thGrade/11thGrade/12thGrade')
+#os.makedirs('Student/11thGrade/12thGrade')
+
+f=open('student.txt.txt','a')
+f.write('Zemzme')
+print(f.read())
+
